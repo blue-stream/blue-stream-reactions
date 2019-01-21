017d0e35358beaafdda22a85f47d01f75cfc4edc
Ron Borysovski <roni537@gmail.com>
2019-01-21 17:41:10 +0200

    refactor: Add boilerplate for exposed procedure

---
+++ src/utils/rpc.server.ts
@@ -0,0 +1,7 @@
+const jayson = require('jayson/promise');
+
+export const RPCServer = new jayson.Server({
+    async getReactionsByResources(resources: string[]) {
+        // TODO: Call your function here and return desired output.
+    },
+});
