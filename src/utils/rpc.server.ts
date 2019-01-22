bf5f6fce0641a01e663c55596cfa2d0f75ed8395
Almog Vagman Ciprut <almogvc@gmail.com>
2019-01-22 11:39:47 +0200

    feat: Add rpc for getAllTypesAmounts

--- src/utils/rpc.server.ts
+++ src/utils/rpc.server.ts
@@ -1,7 +1,9 @@
+import { ReactionManager } from '../reaction/reaction.manager';
+
 const jayson = require('jayson/promise');
 
 export const RPCServer = new jayson.Server({
-    async getReactionsByResources(resources: string[]) {
-        // TODO: Call your function here and return desired output.
+    getReactionsByResources(resources: string[]) {
+        return ReactionManager.getAllTypesAmountsOfResource(resources);
     },
 });
