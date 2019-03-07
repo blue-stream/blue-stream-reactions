1b773428d3c300835126fcd99912e327b35325d7
Yuval Assa <assa.yuval107@gmail.com>
2019-03-07 12:12:15 +0200

    feat: Add health check

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { ReactionRouter } from './reaction/reaction.router';
+import { HealthRouter } from './utils/health/health.router';
 
 const AppRouter: Router = Router();
 
 AppRouter.use('/api/reaction', ReactionRouter);
+AppRouter.use('/health', HealthRouter);
 
 export { AppRouter };
