e6b06e075d5ef3c90dd50b1b8eea606d70a173ef
Yuval Assa <assa.yuval107@gmail.com>
2019-03-07 12:11:50 +0200

    tests: Change db env connection string

--- src/reaction/reaction.router.spec.ts
+++ src/reaction/reaction.router.spec.ts
@@ -87,7 +87,7 @@ describe('Reaction Router', function () {
 
     before(async function () {
 
-        await mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`, { useNewUrlParser: true });
+        await mongoose.connect(config.db.connectionString, { useNewUrlParser: true });
         server = Server.bootstrap();
     });
 
